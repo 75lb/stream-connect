ce31cc7cb21352a745d68048333dba250dc71784
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-12-24 18:52:35 +0000

    switch to js standard style

--- test/test.js
+++ test/test.js
@@ -1,35 +1,34 @@
-var test = require("tape");
-var streamConnect = require("../");
-var PassThrough = require("stream").PassThrough;
+var test = require('tape')
+var streamConnect = require('../')
+var PassThrough = require('stream').PassThrough
 
-test("does not pass through pass2", function(t){
-    t.plan(1);
-    var pass1 = PassThrough();
-    pass1.on("data", function(data){
-        t.strictEqual(data.toString(), "testing");
-    });
+test('does not pass through pass2', function (t) {
+  t.plan(1)
+  var pass1 = PassThrough()
+  pass1.on('data', function (data) {
+    t.strictEqual(data.toString(), 'testing')
+  })
 
-    var pass2 = PassThrough();
-    pass2.on("data", function(data){
-        t.fail("should not fire");
-    });
+  var pass2 = PassThrough()
+  pass2.on('data', function (data) {
+    t.fail('should not fire')
+  })
 
-    pass1.end("testing");
-});
+  pass1.end('testing')
+})
 
-test("when connected, it does pass through pass2", function(t){
-    t.plan(2);
-    var pass1 = PassThrough();
-    pass1.on("data", function(data){
-        t.strictEqual(data.toString(), "testing");
-    });
+test('when connected, it does pass through pass2', function (t) {
+  t.plan(2)
+  var pass1 = PassThrough()
+  pass1.on('data', function (data) {
+    t.strictEqual(data.toString(), 'testing')
+  })
 
-    var pass2 = PassThrough();
-    pass2.on("data", function(data){
-        t.strictEqual(data.toString(), "testing");
-    });
-    
-    var connected = streamConnect(pass1, pass2);
-    connected.end("testing");
-});
+  var pass2 = PassThrough()
+  pass2.on('data', function (data) {
+    t.strictEqual(data.toString(), 'testing')
+  })
 
+  var connected = streamConnect(pass1, pass2)
+  connected.end('testing')
+})
